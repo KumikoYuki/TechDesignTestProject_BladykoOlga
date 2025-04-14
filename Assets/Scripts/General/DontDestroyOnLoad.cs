ee80bcbed565a359d6ceb2570c3957a77ae5702c
Danila Bladyko <d.bladyko@kefirgames.com>
2025-04-14 11:17:39 +0300

    Add Some improvements

--- Assets/Scripts/General/DontDestroyOnLoad.cs
+++ Assets/Scripts/General/DontDestroyOnLoad.cs
@@ -4,6 +4,17 @@ namespace MainMenu
 {
     public class DontDestroyOnLoad : MonoBehaviour
     {
+	private void Awake()
+	{
+	    var count = Object.FindObjectsOfType<DontDestroyOnLoad>().Length;
+
+	    if(count > 1)
+            {
+		Destroy(gameObject);
+
+	    }
+	}
+
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
